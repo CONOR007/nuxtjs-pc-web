86042b579ece5e1eb6af71589a08a0e558e7ce4e
xkwg <xkwg@163.com>
2020-10-19 14:31:35 +0800

    端口号变更

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,8 @@ module.exports = {
   },
 
   server: {
+      port: 9090, // default: 3000
+      host: '0.0.0.0', // default: localhost,
 	    https: {
 	      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
 	      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt'))
