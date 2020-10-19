66c3880293232698a784b9f80960fe4745268a3c
xkwg <xkwg@163.com>
2020-10-19 15:59:08 +0800

    v0.2.7

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ module.exports = {
   },
 
   server: {
-      port: 3000, // default: 3000
+      port: 80, // default: 3000
       host: '0.0.0.0', // default: localhost,
 	    // https: {
 	    //   key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
